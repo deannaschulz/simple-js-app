6378270477e7df19c896ba8c6f862c2b62e81f77
deannaschulz <132223564+deannaschulz@users.noreply.github.com>
2023-05-14 17:18:33 -0500

    Created pokemonlist
    Listed all Pokemon names, heights, and types.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
-alert('Hello world');
-let favoriteFood = 'Tacos';
-document.write(favoriteFood);
+let pokemonList = [
+    { name: "Bulbasaur", height: "7", type: ["grass", "poison"]},
+    { name: "Charizard", height: "11", type: ["fire", "flying"]},
+    { name: "Jigglypuff", height: "5", type: ["fairy", "normal"]},
+    { name: "Meowth", height: "4", type: "normal"},
+    { name: "Psyduck", height: "8", type: "water"},
+    { name: "Pikachu", height: "4", type: "electric"}
+  ];
