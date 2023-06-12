73c96e8aa22650f39f8fc411d00c62555953d31f
deannaschulz <132223564+deannaschulz@users.noreply.github.com>
2023-06-11 21:41:27 -0500

    Change to forEach and added IIFE
    Changed for loop to forEach. Added IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,48 @@
-let pokemonList = [
-    { name: "Bulbasaur", height: "7", type: ["grass", "poison"]},
-    { name: "Charizard", height: "11", type: ["fire", "flying"]},
-    { name: "Jigglypuff", height: "5", type: ["fairy", "normal"]},
-    { name: "Meowth", height: "4", type: "normal"},
-    { name: "Psyduck", height: "8", type: "water"},
-    { name: "Pikachu", height: "4", type: "electric"}
-  ];
-  for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height >=8){
-        document.write(pokemonList[i].name + pokemonList[i].height + "- Wow, that’s big!" + "<br/>");
-    }else if (pokemonList[i].height <8 && pokemonList[i].height >=5){
-        document.write(pokemonList[i].name + pokemonList[i].height + "- This is an average size" + "<br/>");
-    } else {
-        document.write(pokemonList[i].name + pokemonList[i].height + "- This is very small!" + "<br/>");
+let pokemonRepository = (function () {
+let pokemonList = [];
+return {
+    add: function(pokemon) {
+      pokemonList.push(pokemon);
+    },
+    getAll: function() {
+      return pokemonList;
     }
-    }
+  };
+})();
+console.log(pokemonRepository.getAll()); 
+pokemonRepository.add({ name: 'Charmander' });
+console.log(pokemonRepository.getAll());
+    {
+        name: "Bulbasaur",
+        height: "7",
+        type: ["grass", "poison"]
+     },
+ {
+    name: "Charizard",
+    height: "11",
+    type: ["fire", "flying"]
+ },
+ {
+    name: "Jigglypuff",
+    height: "5",
+    type: ["fairy", "normal"]
+ },
+ {
+    name: "Meowth",
+    height: "4",
+    type: ["normal"]
+ },
+ {
+    name: "Psyduck",
+    height: "8",
+    type: ["water"]
+ },
+ {
+    name: "Pikachu",
+    height: "4",
+    type: ["electric"]
+ };
+
+  pokemonList.forEach(function(pokemon) {
+    console.log(pokemon.name + ' is ' + pokemon.height + 'and' + pokemon.type);
+  });
