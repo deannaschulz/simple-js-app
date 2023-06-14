53545cecb6139386bbf15f41e64940ae6ae230e3
deannaschulz <132223564+deannaschulz@users.noreply.github.com>
2023-06-14 17:43:23 -0500

    Fixed error code in index and js

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,48 @@
 let pokemonRepository = (function () {
-let pokemonList = [
-document.write(pokemonRepository.getAll()); 
-pokemonRepository.add({ name: 'Charmander' });
-document.write(pokemonRepository.getAll());
-    {
-        name: "Bulbasaur",
-        height: "7",
-        type: ["grass", "poison"]
-     },
- {
-    name: "Charizard",
-    height: "11",
-    type: ["fire", "flying"]
- },
- {
-    name: "Jigglypuff",
-    height: "5",
-    type: ["fairy", "normal"]
- },
- {
-    name: "Meowth",
-    height: "4",
-    type: ["normal"]
- },
- {
-    name: "Psyduck",
-    height: "8",
-    type: ["water"]
- },
- {
-    name: "Pikachu",
-    height: "4",
-    type: ["electric"]
- }
- ]; 
- return {
-   add: function(pokemon) {
-     pokemonList.push(pokemon);
-   },
-   getAll: function() {
-     return pokemonList;
-   },
- };
+   let pokemonList = [
+      {
+         name: "Bulbasaur",
+         height: "7",
+         type: ["grass", "poison"]
+      },
+      {
+         name: "Charizard",
+         height: "11",
+         type: ["fire", "flying"]
+      },
+      {
+         name: "Jigglypuff",
+         height: "5",
+         type: ["fairy", "normal"]
+      },
+      {
+         name: "Meowth",
+         height: "4",
+         type: ["normal"]
+      },
+      {
+         name: "Psyduck",
+         height: "8",
+         type: ["water"]
+      },
+      {
+         name: "Pikachu",
+         height: "4",
+         type: ["electric"]
+      }
+   ];
+   return {
+      add: function (pokemon) {
+         pokemonList.push(pokemon);
+      },
+      getAll: function () {
+         return pokemonList;
+      },
+   };
 })();
 
- pokemonRepository.add ({ name: "Charmander"});
+pokemonRepository.add({ name: "Charmander" });
 
- pokemonRepository.getAll().forEach(function (pokemon) {
-   document.write (pokemon.name + "is" + pokemon.height + "and" + pokemon.type);
- });
+pokemonRepository.getAll().forEach(function (pokemon) {
+   document.write(pokemon.name + "is" + pokemon.height + "and" + pokemon.type);
+});
